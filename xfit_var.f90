1341a0a9bd1fd8a4e148e0197f708105eab00eb4
Beliavsky <beliavsky@aol.com>
2025-05-07 12:01:20 -0400

    removed commented out code

--- xfit_var.f90
+++ xfit_var.f90
@@ -8,10 +8,6 @@ real(kind=dp), allocatable :: x(:,:)
 integer, parameter :: nrows=10**6, ncols=2
 real(kind=dp), parameter :: var_coeff_1(ncols, ncols) = &
    reshape([3, -4, -2, 5], [ncols, ncols]) / 10.0_dp
-! x = random_normal(nrows, ncols)
-! do i=2,nrows
-!    x(i,:) = x(i,:) + matmul(var_coeff_1, x(i-1,:))
-! end do
 x = sim_vector_ar(nrows, var_coeff_1)
 call print_stats(x)
 call fit_vector_ar(x, var_order=2)
